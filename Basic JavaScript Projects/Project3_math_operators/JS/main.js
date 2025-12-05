af1d4469e1da867d72277b3d70a79bd825e7862f
acliang1412 <acliang7229@gmail.com>
2025-12-04 19:53:08 -0600

    Add increment, decrement, and random number generation functions to main.js

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -39,3 +39,24 @@ function negation_Operator() {
     document.getElementById("Math").innerHTML = -x;
 }
 
+// This function demonstrates the increment operator
+function increment_Operator() {
+    var X = 5;
+    X++; // This increases the value of X by 1
+    document.getElementById("Math").innerHTML = "5 incremented is " + X;
+}
+
+// This function demonstrates the decrement operator
+function decrement_Operator() {
+    var Y = 5.25;
+    Y--; // This decreases the value of Y by 1
+    document.getElementById("Math").innerHTML = "5.25 decremented is " + Y;
+}
+
+// This function generates a random number and displays it in the paragraph
+function generate_Random() {
+    document.getElementById("Math").innerHTML = "Here is a random number between 0 and 100: " + (Math.random() * 100);
+}
+
+// Display a random number between 1 and 10 in an alert when the page loads
+window.alert("Random number from 1 to 10: " + Math.floor(Math.random() * 10 + 1));
