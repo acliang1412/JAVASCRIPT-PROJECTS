17aa661b80b785027d726b1978f9400ae178e78b
acliang1412 <acliang7229@gmail.com>
2025-12-04 19:29:58 -0600

    Add main.js and index.html with addition and subtraction functions

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,12 @@
+// This function is called when the paragraph with the id "Math" is clicked.
+function performCalculation() {
+    // Perform an addition operation
+    var result = 5 + 7;
+    // Display the result in the paragraph element
+    document.getElementById("Math").innerHTML = "5 + 7 = " + result;
+}
+
+function subtraction_Function() {
+}    var Subtraction = 10 - 5;
+    document.getElementById("Math").innerHTML = "10 - 5 = " + Subtraction;
+    
