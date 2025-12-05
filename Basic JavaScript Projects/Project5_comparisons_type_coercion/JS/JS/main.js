e58f032357feb280f86074d7098b7b3caf895c1d
acliang1412 <acliang7229@gmail.com>
2025-12-04 20:08:58 -0600

    Add functionality to display Infinity and -Infinity in main.js and update index.html structure

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/JS/main.js
@@ -8,4 +8,19 @@ document.write("<br><br>");
 
 // Utilize isNaN() to display "false" by checking a number
 document.write("Is 500 a number? ");
-document.write(isNaN(500));
+document.write(isNaN(500) + "<br><br>");
+
+// Use the greater-than operator to display "true"
+document.write("10 > 4 evaluates to: ");
+document.write(10 > 4);
+document.write("<br><br>");
+
+// Use the less-than operator to display "false"
+document.write("10 < 4 evaluates to: ");
+document.write(10 < 4);
+
+// Display Infinity and -Infinity using getElementById
+// A number larger than 1.7976931348623157E308 (Number.MAX_VALUE) displays as Infinity.
+document.getElementById("infinity").innerHTML = 2E308;
+// A negative number smaller than -Number.MAX_VALUE displays as -Infinity.
+document.getElementById("neg_infinity").innerHTML = -3E308;
