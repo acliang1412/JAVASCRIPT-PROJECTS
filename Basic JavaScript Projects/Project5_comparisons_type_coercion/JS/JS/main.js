697b4ef2bdd2d8c39dc898273aac3cb54e90de93
acliang1412 <acliang7229@gmail.com>
2025-12-08 17:43:42 -0600

    Refactor comparison logic in main.js to enhance clarity and structure

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/JS/main.js
@@ -17,10 +17,25 @@ document.write("<br><br>");
 
 // Use the less-than operator to display "false"
 document.write("10 < 4 evaluates to: ");
-document.write(10 < 4);
+document.write(10 < 4 + "<br><br>");
+
+// Use the == operator to return "true"
+document.write("Does 10 equal '10' with type coercion? ");
+document.write(10 == "10");
+document.write("<br><br>");
+
+// Use the == operator to return "false"
+document.write("Does 5 equal 10? ");
+document.write(5 == 10);
 
 // Display Infinity and -Infinity using getElementById
 // A number larger than 1.7976931348623157E308 (Number.MAX_VALUE) displays as Infinity.
 document.getElementById("infinity").innerHTML = 2E308;
 // A negative number smaller than -Number.MAX_VALUE displays as -Infinity.
 document.getElementById("neg_infinity").innerHTML = -3E308;
+
+// Perform a math operation with console.log()
+console.log(8 * 4);
+
+// Display "false" in the console using Boolean logic
+console.log(5 > 10);
