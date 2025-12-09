9fcf764cdcd90be20d0f0432721ebefb3c3fee38
acliang1412 <acliang7229@gmail.com>
2025-12-08 20:41:13 -0600

    Add Time_function to display current time of day and update HTML structure

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -63,3 +63,17 @@ function checkVotingAge() {
     // It's generally better to avoid special characters like '?' in HTML id attributes.
     document.getElementById("How_old_are_you?").innerHTML = votingMessage;
 }
+
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    } else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_Day").innerHTML = Reply;
+}
