fab21118ccd753129ff6deb324076334c511c4ff
acliang1412 <acliang7229@gmail.com>
2025-12-09 20:50:34 -0600

    Refactor while loop implementation and update HTML button functionality

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,16 +1,11 @@
-// Basic while-loop example
-function whileLoopExample(limit) {
-	if (typeof limit !== 'number' || limit <= 0) {
-		return '';
-	}
-	let i = 0;
-	const result = [];
-	while (i < limit) {
-		result.push(i);
-		i++;
-	}
-	return result.join(', ');
-}
 
-// Example usage (uncomment to test in the browser console)
-// console.log(whileLoopExample(5)); // "0, 1, 2, 3, 4"
+// A simple while loop for counting
+function myLoop() {
+	let Digit = "";
+	let X = 1;
+	while (X < 6) {
+		Digit += "<br>" + X;
+		X++;
+	}
+    return Digit;
+}
