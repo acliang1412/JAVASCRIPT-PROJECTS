c57577c8f9b92842a01a220be9ca4316d44f179a
acliang1412 <acliang7229@gmail.com>
2025-12-18 19:01:25 -0600

    Add color output functionality to Color_Function and update canvas rendering

--- Advance JavaScript Projects/JavaScript/JavaScript_1.js
+++ Advance JavaScript Projects/JavaScript/JavaScript_1.js
@@ -5,25 +5,32 @@ function Color_Function() {
     switch(Colors) {
         case "Red":
             Color_Output = "Red" + Color_String;
+            document.getElementById("Output").style.color = "red";
             break;
         case "Yellow":
             Color_Output = "Yellow" + Color_String;
+            document.getElementById("Output").style.color = "yellow";
             break;
         case "Green":
             Color_Output = "Green" + Color_String;
+            document.getElementById("Output").style.color = "green";
             break;
         case "Blue":
             Color_Output = "Blue" + Color_String;
+            document.getElementById("Output").style.color = "blue";
             break;
         case "Pink":
             Color_Output = "Pink" + Color_String;
+            document.getElementById("Output").style.color = "pink";
             break;
 
         case "Purple":
             Color_Output = "Purple" + Color_String;
+            document.getElementById("Output").style.color = "purple";
             break;
         default:
             Color_Output = "Please enter a color exactly as written on the above list.";
+            document.getElementById("Output").style.color = "black";
     }
     document.getElementById("Output").innerHTML = Color_Output;
 }
@@ -32,3 +39,11 @@ function Hello_World_Function() {
     var A = document.getElementsByClassName("Click");
     A[0].innerHTML = "The text has changed!";
 }
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var grd = ctx.createLinearGradient(0, 0, 170, 0);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "white");
+ctx.fillStyle = grd;
+ctx.fillRect(20, 20, 150, 100);
